7b3eb68ad222d313d84b4fac52ce73b29d092be4
DaMexicanJustice <mrrichtofen@gmail.com>
2019-09-01 22:30:24 +0200

    m

--- dmjcode.js
+++ dmjcode.js
@@ -14,12 +14,10 @@ fetch('https://api.twitch.tv/helix/users/follows?to_id=101846410', {
         'Client-ID': 'kokyco38mg96als9lyu4vu7hvlnge2',
         'User-Agent': 'PostmanRuntime/7.16.3',
         'Accept': '*/*',
-        'Cache-Control': 'no-cache',
         'Postman-Token': '7660d411-3de9-44b3-87ab-45389103d5a1,f47ca776-f77a-4584-846f-04b4e54d70d1',
         'Host': 'api.twitch.tv',
         'Accept-Encoding': 'gzip, deflate',
         'Connection': 'keep-alive',
-        'cache-control': 'no-cache'
     }
 }) // Call the fetch function passing the url of the API as a parameter
 .then(function(res) {
