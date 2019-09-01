c0b703dc2d0ac772b9ed8155528f02d055765214
DaMexicanJustice <mrrichtofen@gmail.com>
2019-09-01 21:38:41 +0200

    m

--- dmjcode.js
+++ dmjcode.js
@@ -10,9 +10,9 @@ fetch(url, {
 }) // Call the fetch function passing the url of the API as a parameter
 .then(function(res) {
     // Your code for handling the data you get from the API
-    console.log(res);
+    console.log("Then:" + res);
 })
 .catch(function(err) {
     // This is where you run code if the server returns any errors
-    console.log(err);
+    console.log("Catch: " + err);
 });
