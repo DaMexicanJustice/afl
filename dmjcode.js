41268e9d31e24c98aa15f87d4d460733524d2042
DaMexicanJustice <mrrichtofen@gmail.com>
2019-09-01 21:28:14 +0200

    m

--- dmjcode.js
+++ dmjcode.js
@@ -2,7 +2,12 @@ let channelID = '101846410';
 let channelName = 'aaron_';
 let url = 'https://api.twitch.tv/kraken/channels/' + channelID + '/follows';
 
-fetch(url) // Call the fetch function passing the url of the API as a parameter
+fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+        'Client-ID': '101846410'
+    })
+}) // Call the fetch function passing the url of the API as a parameter
 .then(function(res) {
     // Your code for handling the data you get from the API
     console.log(res);
