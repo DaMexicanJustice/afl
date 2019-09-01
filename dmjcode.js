eb466158cb7b4050e97f0f4060f94d72cd271aa8
DaMexicanJustice <mrrichtofen@gmail.com>
2019-09-01 20:54:49 +0200

    Changed user ID

--- dmjcode.js
+++ dmjcode.js
@@ -1,4 +1,4 @@
-fetch("https://api.twitch.tv/helix/users/follows?from_id=_Aaron") // Call the fetch function passing the url of the API as a parameter
+fetch("https://api.twitch.tv/helix/users/follows?from_id=aaron_") // Call the fetch function passing the url of the API as a parameter
 .then(function(res) {
     // Your code for handling the data you get from the API
     console.log(res);
