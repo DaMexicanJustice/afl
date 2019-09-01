e890143c21f22033935cc796c6a38484cfd88203
DaMexicanJustice <mrrichtofen@gmail.com>
2019-09-01 20:50:23 +0200

    JavaScript file

---
+++ dmjcode.js
@@ -0,0 +1,9 @@
+fetch("https://api.twitch.tv/helix/users/follows?from_id=<_Aaron") // Call the fetch function passing the url of the API as a parameter
+.then(function(res) {
+    // Your code for handling the data you get from the API
+    console.log(res);
+})
+.catch(function(err) {
+    // This is where you run code if the server returns any errors
+    console.log(err);
+});
