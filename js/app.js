b7d5c0c75cb122dc0f20a32460a8664cf0d75602
DaMexicanJustice <mrrichtofen@gmail.com>
2020-01-08 21:13:24 +0100

    Retro site

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBMG-wOi-M43KII-za6C8fAO56Jd_Wphc0",
+    authDomain: "vintage-games-dk.firebaseapp.com",
+    databaseURL: "https://vintage-games-dk.firebaseio.com",
+    projectId: "vintage-games-dk",
+    storageBucket: "vintage-games-dk.appspot.com",
+    messagingSenderId: "761347593076",
+    appId: "1:761347593076:web:7e5801b3eb0e94e34ad221",
+    measurementId: "G-2BG2L826ME"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+function signInGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log("Inside", result);
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        console.log("Reached", token, user);
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
