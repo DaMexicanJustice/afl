5607cdd892f1c51dc1d52f931c323431ddca7f5b
DaMexicanJustice <mrrichtofen@gmail.com>
2020-01-12 20:05:56 +0100

    Retro site

--- js/app.js
+++ js/app.js
@@ -16,28 +16,23 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+function signIn() {
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 
-function signInGoogle() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        console.log("Inside", result);
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        console.log("Reached", token, user);
-        window.location = "https://damexicanjustice.github.io/afl/dashboard.html";
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      window.location('https://damexicanjustice.github.io/afl/dashboard.html'):
+    } else {
+      // No user is signed in.
+    }
+  });
+
 }
 
 function authenticate() {
@@ -51,6 +46,7 @@ function authenticate() {
 }
 
 function signOut() {
-    firebase.auth().signOut();
-    authenticate();
+    firebase.auth().signOut().then(function(){
+      authenticate();
+    });
 }
