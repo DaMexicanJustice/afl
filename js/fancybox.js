8865eb4c8342cf060664476af57604a77877d979
DaMexicanJustice <mrrichtofen@gmail.com>
2020-01-29 15:44:40 +0100

    Retro site

---
+++ js/fancybox.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    $("a#image1").fancybox({
+
+    });			
+    $("a#image2").fancybox({
+
+    });			
+    $("a#image3").fancybox({
+
+    });			
+    $("a#image4").fancybox({
+
+    });			
+    $("a#image5").fancybox({
+
+    });			
+    $("a#image6").fancybox({
+
+    });			
+    $("a#image7").fancybox({
+
+    });			
+    $("a#image8").fancybox({
+
+    });
+    $("a#image9").fancybox({
+
+    });
+    $("a#image10").fancybox({
+
+    });
+    $("a#image11").fancybox({
+
+    });
+    $("a#image12").fancybox({
+
+    });
+    
+    /*
+    $("a[rel=example_group]").fancybox({
+        'transitionIn'		: 'none',
+        'transitionOut'		: 'none',
+        'titlePosition' 	: 'over',
+        'width'             : '960',
+        'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+    */
+
+});
