a07fa76698ed1ef8603665c979fb4240691e2e03
DaMexicanJustice <mrrichtofen@gmail.com>
2020-02-18 20:07:52 +0100

    Retro site

--- js/app.js
+++ js/app.js
@@ -19,18 +19,25 @@ if (!firebase.apps.length) {
 
 var database = firebase.database();
 
-function signIn() {
-  var userEmail = document.getElementById("loginEmail").value;
-  var userPassword = document.getElementById("loginPwd").value;
+function signIn(userEmail, userPassword) {
   firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then(function(user) {
     //then
-    document.cookie = "email="+userEmail+"; mood=Happy";
-    console.log(document.cookie);
+    document.cookie = "email="+userEmail+"; pwd="+userPassword;
     window.location = "./dashboard.html";
   }).catch(function(error) {
     alert("Forkert brugernavn eller password");
     window.location = "./login.html";
   });
+}
+
+function signInWithForm() {
+  var userEmail = document.getElementById("loginEmail").value;
+  var userPassword = document.getElementById("loginPwd").value;
+  signIn(userEmail, userPassword);
+}
+
+function signInWithoutForm(userEmail, userPassword) {
+  signIn(userEmail, userPassword);
 }
 
 function signOut() {
@@ -40,12 +47,32 @@ function signOut() {
 }
 
 function isAuthorized() {
-  console.log(document.cookie);
-  if (!document.cookie.search("vintagegamesdkfirebase@gmail.com")) {
-      window.location = "./login.html";
-  } else {
-    return true;
+  return checkCookie("email") != "" && checkCookie("pwd") != "" ? true : false; 
+}
+
+function checkCookie(credential) {
+  // Get cookie using our custom function
+  return getCookie(credential);
+}
+
+function getCookie(credential) {
+  // Split cookie string and get all individual name=value pairs in an array
+  var cookieArr = document.cookie.split(";");
+  
+  // Loop through the array elements
+  for(var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+      
+      /* Removing whitespace at the beginning of the cookie name
+      and compare it with the given string */
+      if(name == cookiePair[0].trim()) {
+          // Decode the cookie value and return
+          return decodeURIComponent(cookiePair[1]);
+      }
   }
+  
+  // Return null if not found
+  return null;
 }
 
 function checkVersionOfWebApp() {
